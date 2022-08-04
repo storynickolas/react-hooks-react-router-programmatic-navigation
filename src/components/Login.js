242b635707916e328ce8d8906b1e5c03b39d94b3
Nick Story <storynickolasl@gmail.com>
2022-08-04 06:50:44 -0400

    commit tree ca74ab9d03131086ac5dc2b4a8282141d69c1844

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,12 +17,21 @@ function Login({ setIsLoggedIn }) {
 
   function handleSubmit(e) {
     e.preventDefault();
-
-    setIsLoggedIn(true);
-
-    // after logging the user in, redirect to the home page!
-    history.push("/");
+    fetch("http://localhost:3001/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((r) => r.json())
+      .then((user) => {
+        setIsLoggedIn(user);
+        // after logging the user in, redirect to the home page!
+        history.push("/home");
+      });
   }
+
 
   return (
     <form onSubmit={handleSubmit}>
